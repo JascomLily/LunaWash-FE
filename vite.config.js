78874c53ec8725bd0367df2f5b7d7b9e7f36defe
JascomLily <kduy8383@gmail.com>
2026-06-13 07:28:28 +0700

    .vs redundancy measurement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,6 +12,9 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
+    },
+    watch: {
+      ignored: ['**/.vs/**'] 
     }
   }
 })
